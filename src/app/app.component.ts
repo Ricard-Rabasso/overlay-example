df31059b8ef3c235e85e220df62000ef8ba2af0a
Ricard Rabassó <sepm6285@anticipa.com>
2023-03-31 15:10:20 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isOpen = false;
   title = 'overlay-example';
+
+  showOverlay(){
+    this.isOpen = true;
+  }
+
+  close(){
+    this.isOpen = false;
+  }
 }
